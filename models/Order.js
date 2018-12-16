a20370a488a98684374c5590de271d2ee07ca48d
gabrielchase <gchase.patron@gmail.com>
2018-12-17 00:10:46 +0800

    Create payments after order is saved

--- models/Order.js
+++ models/Order.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const Package = require('./Package')
+const Payment = require('./Payment')
 
 const Order = new mongoose.Schema({
     partner_name: { type: String, required: true },
@@ -13,9 +15,35 @@ const Order = new mongoose.Schema({
     status: { type: String, enum: ['In Progress', 'Completed'], default: 'In Progress' },
     description: { type: String },
 
-    created_on: { type: String, default: new Date() },
-    modified_on: { type: String },
-    deleted_on: { type: String }
+    created_on: { type: Date, default: Date.now },
+    modified_on: { type: Date },
+    deleted_on: { type: Date }
+})
+
+Order.post('save', async function(doc) {
+    try {
+        const _package = await Package.findById(doc.package_id)
+    
+        if (!_package)
+            throw new Error(`Package with ID ${doc.package_id} does not exist`)
+        
+        for (let p of _package.payouts) {
+            const order_created_date = doc.created_on
+            let current_date = new Date()
+            const due_date = current_date.setMonth(order_created_date.getMonth() + p.month)
+            
+            const payment = new Payment({
+                partner_name: doc.partner_name,
+                package_id: doc.package_id,
+                due_date, 
+                amount: p.amount
+            })
+
+            await payment.save()
+        }    
+    } catch (error) {
+        throw new Error(`Error creating payments for Order ${doc._id}`)
+    }
 })
 
 module.exports = mongoose.model('Order', Order)
