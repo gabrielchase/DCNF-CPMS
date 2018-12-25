779449949e1dc852c9f6bc4d19c5f3ddae9416d0
gabrielchase <gchase.patron@gmail.com>
2018-12-25 16:50:45 -0700

    Order and payment endpoints

--- routes/order_routes.js
+++ routes/order_routes.js
@@ -1,4 +1,5 @@
 const Order = require('../models/Order')
+const Payment = require('../models/Payment')
 
 const { checkJWT } = require('../lib/middlewares')
 const { success, fail } = require('../lib/json_wrappers')
@@ -14,6 +15,83 @@ module.exports = function(app) {
             await order.save()
 
             success(res, order)
+        } catch (err) {
+            fail(res, err)
+        }
+    })
+
+    // Write test
+    // Change middleware
+    app.get('/api/orders', checkJWT, async (req, res) => {
+        try {
+            const orders = await Order.find({})
+            success(res, orders)
+        } catch (err) {
+            fail(res, err)
+        }
+    })
+
+    // Write test
+    // Change middleware
+    app.get('/api/orders/:order_id', checkJWT, async (req, res) => {
+        const { order_id } = req.params
+        try {
+            const order = await Order.findById(order_id)
+            const payments = await Payment.find({ order_id })
+            success(res, { order, payments })
+        } catch (err) {
+            fail(res, err)
+        }
+    })
+
+    // Write test
+    // Change middleware
+    app.get('/api/payments', checkJWT, async (req, res) => {
+        let { year, month, date } = req.query 
+        
+        year = parseInt(year)
+        month = parseInt(month)
+        date = parseInt(date)
+
+        try {
+            if (month < 1 || month > 12) 
+                throw new Error('Month must be between 1 and 12')
+
+            if (date < 1 || date > 30) 
+                throw new Error('Date must be between 1 and 30')
+
+            let payments = []
+            
+            if (year && !month && !date) {
+                payments =  await Payment.find({ 
+                                due_date: { 
+                                    $gte: new Date(year, 0, 0), 
+                                    $lte: new Date(year, 11, 31) 
+                                }
+                            })
+            } 
+
+            if (year && month && !date) {
+                month = month - 1
+                payments =  await Payment.find({ 
+                    due_date: { 
+                        $gte: new Date(year, month, 1), 
+                        $lte: new Date(year, month, 31) 
+                    }
+                })
+            }
+
+            if (year && month && date) {
+                month = month - 1
+                payments =  await Payment.find({ 
+                    due_date: { 
+                        $gte: new Date(year, month, date), 
+                        $lt: new Date(year, month, date + 1) 
+                    }
+                })
+            }
+
+            success(res, payments)
         } catch (err) {
             fail(res, err)
         }
